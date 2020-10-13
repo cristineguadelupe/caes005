ac5d7d1b8b7432d754cfe0a015423985a17556a4
Cristine Guadelupe <cristineguadelupe@me.com>
2020-10-13 17:13:10 -0300

    First task

--- simple_excercises/hp_kata/lib/hp_kata.ex
+++ simple_excercises/hp_kata/lib/hp_kata.ex
@@ -1,18 +1,10 @@
 defmodule HpKata do
-  @moduledoc """
-  Documentation for `HpKata`.
-  """
 
-  @doc """
-  Hello world.
 
-  ## Examples
-
-      iex> HpKata.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  @spec books_list(map) :: [any]
+  def books_list(books \\ %{}) do
+    books
+    |> Map.keys()
   end
+
 end
--- simple_excercises/hp_kata/test/hp_kata_test.exs
+++ simple_excercises/hp_kata/test/hp_kata_test.exs
@@ -2,7 +2,11 @@ defmodule HpKataTest do
   use ExUnit.Case
   doctest HpKata
 
-  test "greets the world" do
-    assert HpKata.hello() == :world
+  describe "books_list/1" do
+    test "success: get the bought books list" do
+      assert HpKata.books_list(%{1 => 2, 2 => 2}) == [1, 2]
+      assert HpKata.books_list(%{3 => 1, 4 => 1, 5 => 3}) == [3, 4, 5]
+    end
   end
+
 end
