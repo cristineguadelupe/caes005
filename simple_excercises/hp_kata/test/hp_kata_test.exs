7ad1491980df7c6d37bbe3d8aef13355d4732e80
Cristine Guadelupe <cristineguadelupe@me.com>
2020-10-13 17:40:40 -0300

    All discount rules without combinatory

--- simple_excercises/hp_kata/test/hp_kata_test.exs
+++ simple_excercises/hp_kata/test/hp_kata_test.exs
@@ -1,6 +1,5 @@
 defmodule HpKataTest do
   use ExUnit.Case
-  doctest HpKata
 
   describe "books_list/1" do
     test "success: get the bought books list" do
@@ -13,6 +12,15 @@ defmodule HpKataTest do
     test "success: get the full price" do
       assert HpKata.full_price(%{1 => 2, 2 => 1}, 8) == 24
       assert HpKata.full_price(%{3 => 1, 4 => 1, 5 => 3}, 8) == 40
+    end
+  end
+
+  describe "shopping_cart/2" do
+    test "success: get the sale price" do
+      assert HpKata.total(%{1 => 1, 2 => 1}, 8) == 15.20
+    end
+    test "success: get the sale price applying all rules without combinatory"  do
+      assert HpKata.total(%{1 => 2, 2 => 2, 3 => 2, 4 => 1, 5 => 1 } == 51.60)
     end
   end
 
