149ae0fae34842eeb3e94c8f22fdb7d5fd5fae85
Cristine Guadelupe <cristineguadelupe@me.com>
2020-10-21 18:28:03 -0300

    First approach - recursion and struct

--- simple_excercises/atm/lib/atm.ex
+++ simple_excercises/atm/lib/atm.ex
@@ -1,11 +1,38 @@
 defmodule Atm do
 
-  def withdraw(amout) do
-    amout
-    |> valid?()
+  defstruct amount: 0, bills: []
+
+  def withdraw(amount) do
+    __struct__()
+    |> amount(amount)
+    |> bills()
   end
 
-  def valid?(amout) when rem(amout, 10) == 0, do: amout
-  def valid?(_amount), do: 0
+  def amount(atm, amount) do
+    %{atm | amount: valid?(amount)}
+  end
+
+  def bills(atm) when atm.amount == 0 do
+    %{atm | amount: Enum.sum(atm.bills)}
+  end
+  def bills(atm) when atm.amount >= 100 do
+    %{atm | amount: atm.amount - 100, bills: [100 | atm.bills]}
+    |> bills
+  end
+  def bills(atm) when atm.amount >= 50 do
+    %{atm | amount: atm.amount - 50, bills: [50 | atm.bills]}
+    |> bills
+  end
+  def bills(atm) when atm.amount >= 20 do
+    %{atm | amount: atm.amount - 20, bills: [20 | atm.bills]}
+    |> bills
+  end
+  def bills(atm) when atm.amount == 10 do
+    %{atm | amount: atm.amount - 10, bills: [10 | atm.bills]}
+    |> bills
+  end
+
+  defp valid?(amount) when rem(amount, 10) == 0, do: amount
+  defp valid?(_amount), do: 0
 
 end
--- simple_excercises/atm/test/atm_test.exs
+++ simple_excercises/atm/test/atm_test.exs
@@ -3,11 +3,33 @@ defmodule AtmTest do
   doctest Atm
 
   describe "withdraw/1" do
-    test "sucess: returns the withdrawn amount" do
-      assert Atm.withdraw(100) == 100
+    test "success: returns the withdrawn amount" do
+      atm = Atm.withdraw(250)
+      assert atm.amount == 250
+      atm = Atm.withdraw(100)
+      assert atm.amount == 100
     end
-    test "sucess: returns 0 if the amount is invalid" do
-      assert Atm.withdraw(125) == 0
+    test "success: returns 0 if the amount is invalid" do
+      atm = Atm.withdraw(125)
+      assert atm.amount == 0
+      atm = Atm.withdraw(345)
+      assert atm.amount == 0
+    end
+    test "success: returns the correct bills" do
+      atm = Atm.withdraw(250)
+      assert atm.bills == [50, 100, 100]
+      atm = Atm.withdraw(100)
+      assert atm.bills == [100]
+      atm = Atm.withdraw(80)
+      assert atm.bills == [10, 20, 50]
+      atm = Atm.withdraw(30)
+      assert atm.bills == [10, 20]
+    end
+    test "success: retuns an empty list of bills when the amount is invalid" do
+      atm = Atm.withdraw(125)
+      assert atm.bills == []
+      atm = Atm.withdraw(345)
+      assert atm.bills == []
     end
   end
 end
