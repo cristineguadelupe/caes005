b8793a9b97790a5f2c73175bab319446f757db53
Cristine Guadelupe <cristineguadelupe@me.com>
2020-10-21 17:12:29 -0300

    ATM dojo - initial commit

---
+++ simple_excercises/atm/lib/atm.ex
@@ -0,0 +1,18 @@
+defmodule Atm do
+  @moduledoc """
+  Documentation for `Atm`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Atm.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
